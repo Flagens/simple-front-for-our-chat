5fb575eeb3ef30a26f159a03b756c73ff03657d6
Michal <michalwitkowski52@gmail.com>
2023-12-09 10:27:10 +0100

    Update chatbot to utilize ChatGPT API

--- example/urls.py
+++ example/urls.py
@@ -1,8 +1,6 @@
 # example/urls.py
 from django.urls import path
 
-
-
 from . import views
 
 
--- example/views.py
+++ example/views.py
@@ -3,18 +3,30 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from HackathonAI2023 import main
+from django.http import HttpResponse
+
 
 def czat(request):
     return render(request, 'czat.html')
+
+def pytanie(request):
+    #pytanie = request.POST.get('pytanie')
+    pytanie = "dodaj mi do klasy computer parametr aabcd"
+    return pytanie
+
+
 
 @csrf_exempt
 def odpowiedz(request):
     if request.method == 'POST':
         pytanie = request.POST.get('pytanie')
 
-        # Tutaj można dodać logikę odpowiedzi na pytanie
-        odpowiedz = f'Odpowiedź na pytanie: "{pytanie}"'
+        odpowiedz = main.ask_openai_question(pytanie)
+        ai_message = odpowiedz.get('result', '')
 
-        return JsonResponse({'odpowiedz': odpowiedz})
+        # Include both AI Message and the confirmation message in the JSON response
+        #return JsonResponse({'odpowiedz': ai_message, 'confirmation': 'okey i changed it'})
+        return JsonResponse({'odpowiedz' : 'dobra ryju ogarnelem to'})
     else:
         return JsonResponse({'error': 'Nieprawidłowe żądanie'})
